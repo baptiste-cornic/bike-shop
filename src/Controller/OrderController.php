c40fee6db72e10ec2b0662ea092589dfe14c997e
baptiste-cornic <baptiste.cornic92@gmail.com>
2023-01-29 19:05:05 +0100

    Add : stripe + order_validation route

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Entity\OrderProduct;
 use App\Form\ProductType;
 use App\Form\UserInformationType;
 use App\Repository\ProductRepository;
@@ -80,10 +82,13 @@ class OrderController extends AbstractController
         }
 
         $cart = [];
+        $totalPrice = 0;
 
         foreach ($sessionCart as $id => $quantity){
             $product = $productRepo->find($id);
             if($product){
+                $totalPrice = $totalPrice + ($quantity * $product->getPrice());
+
                 $cart[$id]=[
                     'quantity' => $quantity,
                     'product' => $product
@@ -91,8 +96,71 @@ class OrderController extends AbstractController
             }
         }
 
+        \Stripe\Stripe::setApiKey('sk_test_51M7bsdCZpiMH9jLxOZdLAeHz0mJNNMwqMnCPYTBn0qMWRULF9J34YXgaijklermh3CTWRFr3jbFm5W5uO1USPamr00jwUkIY7Z');
+
+        // Create a PaymentIntent with amount and currency
+        $paymentIntent = \Stripe\PaymentIntent::create([
+            'amount' =>  $totalPrice,
+            'currency' => 'eur',
+            'automatic_payment_methods' => [
+                'enabled' => true,
+            ],
+        ]);
+
         return $this->render('order/order_confirmation.html.twig', [
             'cart' => $cart,
+            'clientSecret' => $paymentIntent->client_secret,
+            'totalPrice' => $totalPrice,
         ]);
+    }
+
+    #[Route('/order_validation', name: 'order_validation')]
+    public function orderValidation(ProductRepository $productRepo, EntityManagerInterface $em){
+
+        $user = $this->getUser();
+        if (!$user){
+            $this->addFlash('error', 'Merci de vous connecter pour passer commande.');
+            return $this->redirectToRoute('login');
+        }
+
+        $sessionCart = $this->requestStack->getSession()->get('cart');
+        if (!$sessionCart){
+            $this->addFlash('error', 'Votre panier est vide.');
+            return $this->redirectToRoute('product_list');
+        }
+
+        $totalPrice = 0;
+        $productsTab = [];
+
+        foreach ($sessionCart as $id => $quantity){
+            $product = $productRepo->find($id);
+            $product->quantity = $quantity;
+            if($product){
+                $totalPrice = $totalPrice + ($quantity * $product->getPrice());
+                $productsTab[] = $product;
+            }
+        }
+
+        $order = new Order();
+        $order->setUser($this->getUser())
+            ->setPrice($totalPrice);
+
+        $em->persist($order);
+
+        foreach ($productsTab as $product){
+            $orderProduct = new OrderProduct();
+            $orderProduct->setOrder($order)
+                ->setProduct($product)
+                ->setUnitPrice($product->getPrice())
+                ->setQuantity($product->quantity);
+            $em->persist($orderProduct);
+        }
+
+        $em->flush();
+
+        $this->requestStack->getSession()->remove('cart');
+
+        $this->addFlash('success', 'Paiement effectué avec succès, votre commande vous sera livré prochainement.');
+        return $this->redirectToRoute('product_list');
     }
 }
