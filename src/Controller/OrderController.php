9e0765f1a885b6fd6046b259fff1977766b85e16
baptiste-cornic <baptiste.cornic92@gmail.com>
2023-01-30 15:33:09 +0100

    Add : cartService

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -7,6 +7,7 @@ use App\Entity\OrderProduct;
 use App\Form\ProductType;
 use App\Form\UserInformationType;
 use App\Repository\ProductRepository;
+use App\Service\CartService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\RedirectResponse;
@@ -25,7 +26,7 @@ class OrderController extends AbstractController
     }
 
     #[Route('/order', name: 'order')]
-    public function index(Request $request, ProductRepository $productRepo, EntityManagerInterface $em): Response
+    public function index(Request $request, ProductRepository $productRepo, EntityManagerInterface $em, CartService $cartService): Response
     {
         $user = $this->getUser();
         if (!$user){
@@ -39,17 +40,7 @@ class OrderController extends AbstractController
             return $this->redirectToRoute('product_list');
         }
 
-        $cart = [];
-
-        foreach ($sessionCart as $id => $quantity){
-            $product = $productRepo->find($id);
-            if($product){
-                $cart[$id]=[
-                    'quantity' => $quantity,
-                    'product' => $product
-                ];
-            }
-        }
+        $cart = $cartService->getCart();
 
         $form = $this->createForm(UserInformationType::class, $user);
         $form->handleRequest($request);
@@ -62,12 +53,13 @@ class OrderController extends AbstractController
 
         return $this->render('order/index.html.twig', [
             'form' => $form->createView(),
-            'cart' => $cart,
+            'cart' => $cart['cart'],
         ]);
     }
 
     #[Route('/order_confirmation', name: 'order_confirmation')]
-    public function orderConfirmation(Request $request, ProductRepository $productRepo, EntityManagerInterface $em): Response
+    public function orderConfirmation(Request $request, ProductRepository $productRepo, EntityManagerInterface $em,
+                                      CartService $cartService): Response
     {
         $user = $this->getUser();
         if (!$user){
@@ -81,26 +73,13 @@ class OrderController extends AbstractController
             return $this->redirectToRoute('product_list');
         }
 
-        $cart = [];
-        $totalPrice = 0;
-
-        foreach ($sessionCart as $id => $quantity){
-            $product = $productRepo->find($id);
-            if($product){
-                $totalPrice = $totalPrice + ($quantity * $product->getPrice());
-
-                $cart[$id]=[
-                    'quantity' => $quantity,
-                    'product' => $product
-                ];
-            }
-        }
+        $cart = $cartService->getCart();
 
         \Stripe\Stripe::setApiKey('sk_test_51M7bsdCZpiMH9jLxOZdLAeHz0mJNNMwqMnCPYTBn0qMWRULF9J34YXgaijklermh3CTWRFr3jbFm5W5uO1USPamr00jwUkIY7Z');
 
         // Create a PaymentIntent with amount and currency
         $paymentIntent = \Stripe\PaymentIntent::create([
-            'amount' =>  $totalPrice,
+            'amount' => $cart['totalPrice'],
             'currency' => 'eur',
             'automatic_payment_methods' => [
                 'enabled' => true,
@@ -108,9 +87,9 @@ class OrderController extends AbstractController
         ]);
 
         return $this->render('order/order_confirmation.html.twig', [
-            'cart' => $cart,
+            'cart' => $cart['cart'],
             'clientSecret' => $paymentIntent->client_secret,
-            'totalPrice' => $totalPrice,
+            'totalPrice' => $cart['totalPrice'],
         ]);
     }
 
