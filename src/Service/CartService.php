1ab8435994ce00f4827c4f4a0ef0a20fb04d0b88
baptiste-cornic <baptiste.cornic92@gmail.com>
2023-01-30 15:33:40 +0100

    Add : new CartService

---
+++ src/Service/CartService.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Service;
+
+use App\Repository\ProductRepository;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+class CartService
+{
+    private ProductRepository $productRepo;
+    private RequestStack $requestStack;
+
+    public function __construct(ProductRepository $productRepo, RequestStack $requestStack,){
+        $this->productRepo = $productRepo;
+        $this->requestStack = $requestStack;
+    }
+
+    public function getCart(): array
+    {
+        $sessionCart = $this->requestStack->getSession()->get('cart');
+        $cart = [];
+        $totalPrice = 0;
+
+        foreach ($sessionCart as $id => $quantity){
+            $product = $this->productRepo->find($id);
+            if($product){
+                $totalPrice = $totalPrice + ($quantity * $product->getPrice());
+                $cart[$id]=[
+                    'quantity' => $quantity,
+                    'product' => $product
+                ];
+            }
+        }
+
+        return ['cart' => $cart, 'totalPrice' => $totalPrice];
+
+    }
+
+}
