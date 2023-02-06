4753f743f94774d075965d35694b7eb5e4b8886e
baptiste-cornic <baptiste.cornic92@gmail.com>
2023-02-06 12:28:02 +0100

    Update : translate error msg

--- src/Form/RegistrationFormType.php
+++ src/Form/RegistrationFormType.php
@@ -29,13 +29,13 @@ class RegistrationFormType extends AbstractType
                 'attr' => ['autocomplete' => 'new-password'],
                 'constraints' => [
                     new NotBlank([
-                        'message' => 'Please enter a password',
+                        'message' => 'Merci de choisir un mot de passe',
                     ]),
                     new Length([
                         'min' => 6,
-                        'minMessage' => 'Your password should be at least {{ limit }} characters',
+                        'minMessage' => 'Votre mot de passe doit contenir au moins {{ limit }} caractères.',
                         // max length allowed by Symfony for security reasons
-                        'max' => 4096,
+                        'max' => 255,
                     ]),
                 ],
             ])
